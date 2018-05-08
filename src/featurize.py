f676e33c7f72c31c78f382b164313cfd273bc3ef
Elliott Crane <ellcrane@gmail.com>
2018-05-08 10:28:02 -0700

    small changes

--- src/featurize.py
+++ src/featurize.py
@@ -26,3 +26,30 @@ def is_food(item):
         return True
     else:
         return False
+
+if __name__ == "__main__":
+    #reads yelp data into dataframe
+    file_path = '~/g/projects/yelp/dataset/business.json'
+    yelp_business_data = create_pandas_df_from_json(file_path)
+
+    #filters businesses that were open when this dataset was published Jan. 2018
+    open_businesses = yelp_business_data[yelp_business_data['is_open'] == 1]
+
+    #creates column that says if business is restaurant and creates df of just open restaurants
+    open_businesses['is_food'] = open_businesses['categories'].apply(is_food)
+    open_restaurants = open_businesses[open_businesses['is_food'] == True]
+
+    #creates column that says if business is in USA and creates df of just
+    #restaurants open in the US as of January 2018
+    states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    open_restaurants['in_US'] = open_restaurants['state'].isin(states)
+    previously_open_US_restaurants = open_restaurants[open_restaurants['in_US'] == True]
+
+
+    client = MongoClient('mongodb://localhost:27017/')
+    restaurants = client['restaurants']
+    google_places = restaurants['google_places']
